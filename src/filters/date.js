b23091338bde192803da2129739dec8686c83687
Peter Bryant <pb@ptrbrynt.com>
2026-05-01 12:08:47 +0100

    Use London time

--- src/filters/date.js
+++ src/filters/date.js
@@ -2,27 +2,20 @@
 // Posted by Brian
 // Retrieved 2026-04-28, License - CC BY-SA 4.0
 
-const { DateTime } = require("luxon");
+const { DateTime, Settings } = require("luxon");
 
 // Add a friendly date filter to nunjucks.
 // Defaults to format of LLLL dd, yyyy unless an alternate is passed as a parameter.
 // {{ date | friendlyDate('OPTIONAL FORMAT STRING') }}
 // List of supported tokens: https://moment.github.io/luxon/docs/manual/formatting.html#table-of-tokens
 
+Settings.defaultLocale = "en-GB";
+
 module.exports = (dateObj, format = "DDD") => {
+  const options = { zone: "Europe/London" };
   if (dateObj instanceof Date) {
-    return DateTime.fromJSDate(dateObj, {
-      zone: "utc",
-      locale: "en",
-    })
-      .setLocale("en-GB")
-      .toFormat(format);
+    return DateTime.fromJSDate(dateObj, options).toFormat(format);
   } else {
-    return DateTime.fromISO(dateObj, {
-      zone: "utc",
-      locale: "en",
-    })
-      .setLocale("en-GB")
-      .toFormat(format);
+    return DateTime.fromISO(dateObj, options).toFormat(format);
   }
 };
