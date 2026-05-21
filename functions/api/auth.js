f0470e4f82259ff75c114708c3a2e15f5af6381b
Peter Bryant <pb@ptrbrynt.com>
2026-05-21 17:08:54 +0100

    Fix Decap config

--- functions/api/auth.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const { searchParams } = new URL(context.request.url);
-  const redirectUrl = new URL("https://github.com/login/oauth/authorize");
-  redirectUrl.searchParams.set("client_id", context.env.GITHUB_CLIENT_ID);
-  redirectUrl.searchParams.set("scope", "repo");
-  const state = searchParams.get("state") || "";
-  redirectUrl.searchParams.set("state", state);
-  return Response.redirect(redirectUrl.toString(), 302);
-}
